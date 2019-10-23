21f5d2cbe1821bfc1fb14055ac6bb91ff17b5c0c
steveoc <steveoc64@gmail.com>
2019-10-23 12:11:32 +1000

    add in-memory database

--- database/memory.go
+++ database/memory.go
@@ -1,1 +1,64 @@
 package database
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	pb "github.com/steveoc64/petstore/proto"
+)
+
+type MemoryDB struct {
+	sync.RWMutex
+	Pets map[int64]*pb.Pet
+}
+
+func NewMemoryDB() *MemoryDB {
+	return &MemoryDB{Pets: make(map[int64]*pb.Pet)}
+}
+
+// GetPetByID returns the pet by the given ID, or nil + error if not found
+func (db *MemoryDB) GetPetByID(ctx context.Context, id int64) (*pb.Pet, error) {
+	// TODO - handle context timeouts.
+	// .. not that they are likely in an in-Memory DB driver, but needed for completeness
+	db.RLock()
+	defer db.RUnlock()
+	pet, ok := db.Pets[id]
+	if !ok {
+		return nil, fmt.Errorf("404:Pet %#v not found", id)
+	}
+	return pet, nil
+}
+
+// UpdatePet upadates the name and status of a pet
+func (db *MemoryDB) UpdatePet(ctx context.Context, id int64, name string, status string) error {
+	db.Lock()
+	db.Unlock()
+	pet, ok := db.Pets[id]
+	if !ok {
+		return fmt.Errorf("405:No Pet %d to update", id)
+	}
+	status = strings.ToLower(status)
+	if _, ok := pb.Status_value[status]; !ok {
+		return fmt.Errorf("405:Invalid status value %#v", status)
+	}
+	pet.Name = name
+	pet.Status = status
+	return nil
+}
+
+// AddPet adds a pet to the database, unless it already exists or is invalid
+func (db *MemoryDB) AddPet(ctx context.Context, pet *pb.Pet) error {
+	db.Lock()
+	defer db.Unlock()
+	if pet.Id == 0 {
+		return errors.New("405:Pet ID 0 is invalid")
+	}
+	if _, ok := db.Pets[pet.Id]; ok {
+		return fmt.Errorf("405:Pet already exists %d", pet.Id)
+	}
+	db.Pets[pet.Id] = pet
+	return nil
+}
